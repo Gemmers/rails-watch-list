7de2e03a1d4bf468a48aaa6497c56ae605fa2d6c
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-07 15:53:39 +0100

    ex3 List.all ok

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,30 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "lists#index"
+
+    get '/lists', to: 'lists#index'
+
+    # get '/some_route', to: 'some_controller#some_action'
+
+    # For listing all restaurants
+    # get '/restaurants', to: 'restaurants#index'
+
+    # For creating a new restaurant
+    # post '/restaurants', to: 'restaurants#create'
+
+    # For rendering a form for creating a new restaurant
+    # get '/restaurants/new', to: 'restaurants#new'
+
+    # For rendering a form for editing an existing restaurant
+    # get '/restaurants/:id/edit', to: 'restaurants#edit'
+
+    # For displaying a specific restaurant
+    # get '/restaurants/:id', to: 'restaurants#show'
+
+    # For updating a specific restaurant
+    # patch '/restaurants/:id', to: 'restaurants#update'
+
+    # For deleting a specific restaurant
+    # delete '/restaurants/:id', to: 'restaurants#destroy'
 end
